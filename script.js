d19751f4af084096b192c150da8d3330ad035e72
reza kouhsari <rezakouhsari@gmail.com>
2024-11-14 11:09:10 +0330

    filter price range

---
+++ script.js
@@ -0,0 +1,54 @@
+const minVal = document.querySelector(".min-val");
+const maxVal = document.querySelector(".max-val");
+const priceInputMin = document.querySelector(".min-input");
+const priceInputMax = document.querySelector(".max-input");
+const range = document.querySelector(".range-track");
+const minGap = 0;
+const sliderMinValue = parseInt(minVal.min);
+const sliderMaxValue = parseInt(maxVal.max);
+
+window.onload = function () {
+  slideMin();
+  slideMax();
+};
+
+function slideMin() {
+  let gap = parseInt(maxVal.value) - parseInt(minVal.value);
+  if (gap <= minGap) {
+    minVal.value = parseInt(maxVal.value) - minGap;
+  }
+  priceInputMin.value = minVal.value;
+  setArea();
+}
+
+function slideMax() {
+  let gap = parseInt(maxVal.value) - parseInt(minVal.value);
+  if (gap <= minGap) {
+    maxVal.value = parseInt(minVal.value) + minGap;
+  }
+  priceInputMax.value = maxVal.value;
+  setArea();
+}
+
+function setArea() {
+  range.style.left = (minVal.value / sliderMaxValue) * 100 + "%";
+  range.style.right = 100 - (maxVal.value / sliderMaxValue) * 100 + "%";
+}
+
+function setMinInput() {
+  let minPrice = parseInt(priceInputMin.value);
+  if (minPrice < sliderMinValue) {
+    priceInputMin.value = sliderMinValue;
+  }
+  minVal.value = priceInputMin.value;
+  slideMin();
+}
+
+function setMaxInput() {
+  let maxPrice = parseInt(priceInputMax.value);
+  if (maxPrice < sliderMaxValue) {
+    priceInputMax.value = sliderMaxValue;
+  }
+  maxVal.value = priceInputMax.value;
+  slideMax();
+}
